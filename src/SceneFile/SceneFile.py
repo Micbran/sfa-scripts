f28338db6a9a13ef073cdbec02efa351694ac953
Micbran <micbran22@gmail.com>
2021-03-01 13:26:31 -0600

    Basic scaffolding for SceneFile object

--- src/SceneFile/SceneFile.py
+++ src/SceneFile/SceneFile.py
@@ -2,5 +2,18 @@ from os import path
 
 
 class SceneFile(object):
-    def __init__(self):
-        pass
+    def __init__(self, folder_path, descriptor, task, version, extension):
+        self.folder_path = folder_path
+        self.descriptor = descriptor
+        self.task = task
+        self.version = version
+        self.extension = extension
+
+    @property
+    def file_name(self):
+        return "{0}_{1}_v{2}.{3}"\
+            .format(self.descriptor, self.task, self.version, self.extension)
+
+    @property
+    def path(self):
+        return path.join(self.folder_path, self.file_name)
