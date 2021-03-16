f43d4163089aede17242a4709ff222e1935fd779
Micbran <micbran22@gmail.com>
2021-03-15 20:29:51 -0500

    Basic changes to make simpleui not be occuluded by maya parent window

--- src/smartsave/simpleui.py
+++ src/smartsave/simpleui.py
@@ -1,9 +1,16 @@
 from PySide2 import QtWidgets
-from .scenefile import SceneFile
+import maya.OpenMayaUI as omui
+from shiboken2 import wrapInstance
+from scenefile import SceneFile
+
+
+def return_maya_main_window():
+	main_window = omui.MQtUtil.mainWindow()
+	return wrapInstance(long(main_window), QtWidgets.QWidget)
 
 
 class SimpleUI(QtWidgets.QDialog):
 
 	def __init__(self):
-		super(SimpleUI, self).__init__()
+		super(SimpleUI, self).__init__(parent=return_maya_main_window())
 		self.setWindowTitle("A Simple UI")
