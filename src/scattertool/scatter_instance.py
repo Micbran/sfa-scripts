734dae80db24ce2390ab759aff7b565aace86429
Micbran <micbran22@gmail.com>
2021-04-08 10:32:53 -0500

    Selection and Scale UI skeleton

--- src/scattertool/scatter_instance.py
+++ src/scattertool/scatter_instance.py
@@ -1,7 +1,6 @@
 import maya.cmds as cmds
 import logging
 import random
-import constants
 
 log = logging.getLogger(__name__)
 random.seed()
@@ -11,17 +10,18 @@ class ScatterInstance(object):
     def __init__(self,
                  source_object,
                  destinations,
-                 scale_ranges=(constants.MIN_SCALE, constants.MIN_SCALE, constants.MIN_SCALE),
-                 rotation_ranges=(constants.MIN_ROTATION, constants.MIN_ROTATION, constants.MIN_ROTATION)):
+                 scale_ranges=((1, 1), (1, 1), (1, 1)),
+                 rotation_ranges=((0, 0), (0, 0), (0, 0))):
+        # let's make Python look like Lisp
         self.source_object = source_object
         self.destinations = destinations
         self.scale_ranges = scale_ranges
         self.rotation_ranges = rotation_ranges
 
-    def scatter_on_source(self):
+    def scatter_on_source(self, square_scale):
         pass
 
-    def _scale_by_randoms(self, instance):
+    def _scale_by_randoms(self, instance, square_scale):
         pass
         # random.uniform()
 
--- src/scattertool/scatterui.py
+++ src/scattertool/scatterui.py
@@ -3,8 +3,11 @@ import maya.OpenMayaUI as omui
 import maya.cmds as cmds
 from shiboken2 import wrapInstance
 from pymel.core.system import Path
-import constants
 from scatter_instance import ScatterInstance
+
+MIN_SCALE = 1.0
+MIN_ROTATION = 0
+MAX_ROTATION = 360
 
 
 def return_maya_main_window():
@@ -41,10 +44,99 @@ class ScatterToolUI(QtWidgets.QDialog):
         self.main_layout.addLayout(self.button_layout)
 
     def _create_selection_ui(self):
-        return QtWidgets.QVBoxLayout()
+        self.source_label = QtWidgets.QLabel("Scatter Source")
+        self.source_label.setStyleSheet("font: bold;")
+        self.destination_label = QtWidgets.QLabel("Scatter Destination")
+        self.destination_label.setStyleSheet("font: bold;")
+
+        self.source_line_edit = QtWidgets.QLineEdit()
+        self.source_line_edit.setMinimumWidth(100)
+        self.destination_text_box = QtWidgets.QTextEdit()
+        self.destination_text_box.setMinimumWidth(150)
+        self.destination_text_box.setMinimumHeight(50)
+
+        self.source_select_button = QtWidgets.QPushButton("Select")
+        self.source_select_button.setMaximumWidth(75)
+        self.destination_select_button = QtWidgets.QPushButton("Select")
+        self.destination_select_button.setMaximumWidth(75)
+
+        selection_grid = QtWidgets.QGridLayout()
+        selection_grid.addWidget(self.source_label, 0, 0)
+        selection_grid.addWidget(self.destination_label, 0, 2)
+        selection_grid.addWidget(self.source_line_edit, 1, 0)
+        selection_grid.addWidget(self.destination_text_box, 1, 2)
+        selection_grid.addWidget(self.source_select_button, 1, 1)
+        selection_grid.addWidget(self.destination_select_button, 1, 3)
+        return selection_grid
 
     def _create_scale_ui(self):
-        return QtWidgets.QVBoxLayout()
+        self.scale_label = QtWidgets.QLabel("Scale Ranges")
+        self.scale_label.setStyleSheet("font: bold;")
+
+        self.scale_x_label = QtWidgets.QLabel("X: ")
+        self.scale_x_label.setStyleSheet("font: bold;")
+        self.scale_y_label = QtWidgets.QLabel("Y: ")
+        self.scale_y_label.setStyleSheet("font: bold;")
+        self.scale_z_label = QtWidgets.QLabel("Z: ")
+        self.scale_z_label.setStyleSheet("font: bold;")
+
+        self.scale_square_label = QtWidgets.QLabel("Square Scale")
+        self.scale_square_label.setStyleSheet("font: bold;")
+        self.scale_xyz_label = QtWidgets.QLabel("XYZ: ")
+        self.scale_xyz_label.setStyleSheet("font: bold;")
+        
+        self.dash_label = QtWidgets.QLabel("-")
+
+        self.scale_x_min = QtWidgets.QLineEdit()
+        self.scale_x_min.setMaximumWidth(50)
+        self.scale_x_max = QtWidgets.QLineEdit()
+        self.scale_x_max.setMaximumWidth(50)
+
+        self.scale_y_min = QtWidgets.QLineEdit()
+        self.scale_y_min.setMaximumWidth(50)
+        self.scale_y_max = QtWidgets.QLineEdit()
+        self.scale_y_max.setMaximumWidth(50)
+
+        self.scale_z_min = QtWidgets.QLineEdit()
+        self.scale_z_min.setMaximumWidth(50)
+        self.scale_z_max = QtWidgets.QLineEdit()
+        self.scale_z_max.setMaximumWidth(50)
+
+        self.scale_square_min = QtWidgets.QLineEdit()
+        self.scale_square_min.setMaximumWidth(50)
+        self.scale_square_min.setDisabled(True)
+        self.scale_square_max = QtWidgets.QLineEdit()
+        self.scale_square_max.setMaximumWidth(50)
+        self.scale_square_max.setDisabled(True)
+
+        self.scale_square_checkbox = QtWidgets.QCheckBox()
+
+        scale_grid = QtWidgets.QGridLayout()
+        scale_grid.addWidget(self.scale_label, 0, 0)
+
+        scale_grid.addWidget(self.scale_x_label, 1, 0)
+        scale_grid.addWidget(self.scale_x_min, 1, 1)
+        scale_grid.addWidget(self.dash_label, 1, 2)
+        scale_grid.addWidget(self.scale_x_max, 1, 3)
+
+        scale_grid.addWidget(self.scale_y_label, 2, 0)
+        scale_grid.addWidget(self.scale_y_min, 2, 1)
+        scale_grid.addWidget(self.dash_label, 2, 2)
+        scale_grid.addWidget(self.scale_y_max, 2, 3)
+
+        scale_grid.addWidget(self.scale_z_label, 3, 0)
+        scale_grid.addWidget(self.scale_z_min, 3, 1)
+        scale_grid.addWidget(self.dash_label, 3, 2)
+        scale_grid.addWidget(self.scale_z_max, 3, 3)
+
+        scale_grid.addWidget(self.scale_square_label, 4, 0)
+        scale_grid.addWidget(self.scale_square_checkbox, 4, 1)
+
+        scale_grid.addWidget(self.scale_square_label, 5, 0)
+        scale_grid.addWidget(self.scale_square_min, 5, 1)
+        scale_grid.addWidget(self.dash_label, 5, 2)
+        scale_grid.addWidget(self.scale_square_max, 5, 3)
+        return scale_grid
 
     def _create_rotation_ui(self):
         return QtWidgets.QVBoxLayout()
@@ -53,6 +145,9 @@ class ScatterToolUI(QtWidgets.QDialog):
         return QtWidgets.QVBoxLayout()
 
     def _create_connections(self):
+        self.source_select_button.clicked.connect(self._get_current_select_single_object)
+        self.destination_select_button.clicked.connect(self._get_current_select_multi_object_vertex)
+        self.scale_square_checkbox.clicked.connect(self._toggle_scale_inputs)
         pass
 
     @QtCore.Slot()
@@ -64,9 +159,13 @@ class ScatterToolUI(QtWidgets.QDialog):
         pass
 
     @QtCore.Slot()
+    def _toggle_scale_inputs(self):
+        pass
+
+    @QtCore.Slot()
     def _do_scatter(self):
         scatter = self._create_scatter_instance_from_fields()
-        scatter.scatter_on_source()
+        scatter.scatter_on_source(square_scale=self.square_tickbox.value())
 
     def _update_scatter_instance_from_fields(self):
         scale_ranges = (float(self.scale_x_edit.text()),
@@ -79,4 +178,3 @@ class ScatterToolUI(QtWidgets.QDialog):
         destinations = self.destinations
         return ScatterInstance(source_object, destinations,
                                scale_ranges, rotation_ranges)
-
